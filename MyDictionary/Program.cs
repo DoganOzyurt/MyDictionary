5cbf43ce395c25e2b1b92739c9ad4bf5c5b90b43
Dogan OZYURT <dogan.ozyurt@sbu.edu.tr>
2021-01-24 23:35:26 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> score = new List<int>();
+            score.Add(100);
+            score.Add(95);
+            score.Add(85);
+            score.Add(120);
+
+
+            Console.WriteLine(score.Count);
+
+            MyList<string> clubname = new MyList<string>();
+            clubname.Add("Galatasaray");
+            clubname.Add("FenerBahce");
+            clubname.Add("Besiktas");
+            clubname.Add("BartinSpor");
+
+            Console.WriteLine(clubname.Count);
+
+
+        }
+    }
+}
